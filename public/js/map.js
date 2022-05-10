6b0006781ada7a0dd9698c02174d7d2e52776d1f
unknown <omar.h_mobarak@yahoo.com>
2022-05-10 11:33:35 +0200

    updated show page/view.. added maps and modified layout a bit.

---
+++ public/js/map.js
@@ -0,0 +1,17 @@
+console.log(coordinates);
+mapboxgl.accessToken = `${mapboxToken}`;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/satellite-v9", // style URL
+  // other styles: streets-v11, satellite-v9, light-v10, dark-v10, outdoors-v11
+  center: coordinates, // starting position [lng, lat]
+  zoom: 2, // starting zoom
+});
+
+new mapboxgl.Marker()
+  .setLngLat(coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+    .setHTML(`<h3>${campground.title}</h3>`)
+  )
+  .addTo(map);
